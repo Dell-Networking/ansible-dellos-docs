653ea0be2638aa09e950e1d98fb35edf1c452e29
Senthil Kumar Ganesan <skg.net.dev@gmail.com>
2016-11-17 17:34:47 -0800

    Removed Github Pages extension

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,6 @@
 extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
